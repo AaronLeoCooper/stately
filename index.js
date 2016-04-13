13e4cfecefe4c1001a55b744c7185ee83c06c2e4
Aaron Cooper <aaron.cooper@2359media.com>
2016-04-13 22:05:39 +0800

    Some initial code in place, type converstions mostly done

--- index.js
+++ index.js
@@ -11,20 +11,81 @@
   /**
    * Private methods
    */
-  // If the store's type matches the new value's type
-  function typesMatch(store, new) {
-    return typeof store === typeof new;
+  // Log Info
+  function log(msg) {
+    console.log(msg);
   }
 
-  // Attempt to convert the new value into the type of the store
-  function mutateType(store, new) {
-    var storeType = typeof store;
+  // Log Warning
+  function warn(msg) {
+    console.warn(msg);
+  }
 
-    switch (storeType) {
-      case 'number':
-        
-        break;
+  // Log Error
+  function err(msg) {
+    console.error(msg);
+  }
+
+  // If the store's type matches the new value's type
+  function typesMatch(store, value) {
+    return typeof store === typeof value;
+  }
+
+  // Checks if the type can be mutated, returns boolean
+  function canMutateType(store, value) {
+    return typeof store === typeof value;
+  }
+
+  // Attempt to convert the new value into the type of the store, retuns newly type-mutated value
+  function mutateType(store, value) {
+    var storeType = typeof store;
+    var valueType = typeof value;
+    var typeConversions = {};
+
+    // If types already match, just return the new value
+    if (storeType === valueType) {
+      return value;
     }
+
+    // Converstion chart, converts from level 1 to level 2
+    typeConversions = {
+      number: {
+        string: function () {
+          return value.toString();
+        },
+        boolean: function () {
+          if (value > 0) {
+            return true;
+          } else {
+            return false;
+          }
+        }
+      },
+      string: {
+        number: function () {
+          if (value.indexOf('.') > -1) { // Has decimal
+            return value.parseFloat();
+          } else { // Is negative
+            return value.parseInt();
+          }
+        },
+        boolean: function () {
+          if (value.lower === 'true') {
+
+          } else {
+
+          }
+        }
+      }
+      boolean: {
+        number: function () {
+
+        },
+        string: function () {
+
+        }
+      }
+    };
   }
 
 
@@ -40,14 +101,32 @@
    * Public methods
    */
   Stately.prototype.set = function (key, value, callback) {
+    var cb;
+
+    switch (typeof callback) {
+      case 'undefined':
+      case 'function':
+        cb = callback || false;
+        break;
+
+      default:
+        warn('A non-function was passed to a Stately.set() callback. I\'ll choose to ignore it for now... But please correct your code soon!');
+        break;
+    }
+
     if (typeof key === 'string') {
-      if (!store.hasOwnProperty(key) || typesMatch(store[key], value)) { // New state property
+      if (!store.hasOwnProperty(key) || typesMatch(store[key], value)) { // New state property, or types match
         store[key] = value;
-      } else { // Existing state property
+      } else if (canMutateType(store, value)) { // Existing state property & types are different & can be type mutated
 
       }
     } else if (typeof key === 'object') {
 
+    }
+
+    // After setting state, run the callback if one was specified
+    if (cb) {
+      cb();
     }
   };
 
